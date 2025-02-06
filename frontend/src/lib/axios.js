604ee9c005fe8b4f3f69e404c7f9ac9d402ba87a
Lisha <marathelisha@gmail.com>
2025-02-06 22:41:00 +0530

    url change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "https://chatbot-o2lo.vercel.app/api",
+      : "https://chatbot-o2lo.vercel.app",
   withCredentials: true,
 });
